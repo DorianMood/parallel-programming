4852126ed5b5193ecdc6f1501d19a83951733763
nekithockey75 <nikita01051997@gmail.com>
2020-04-06 11:56:31 +0500

    Radix  seq

--- lab-1/RadixSorter.cc
+++ lab-1/RadixSorter.cc
@@ -3,32 +3,49 @@
 #include <vector>
 #include <algorithm>
 
+uint64_t getMax(const uint64_t *array, const int array_size)
+{
+    uint64_t m = array[0];
+    for (int i = 0; i < array_size; i++)
+        if (array[i] > m)
+            m = array[i];
+    return m;
+}
+
+void countSort(uint64_t *array, int array_size, uint64_t exp)
+{
+    uint64_t output[array_size];
+    int i;
+    uint64_t count[10] = {0};
+
+    for (i = 0; i < array_size; i++)
+        count[(array[i] / exp) % 10]++;
+
+    for (i = 1; i < 10; i++)
+        count[i] += count[i - 1];
+
+    for (i = array_size - 1; i >= 0; i--)
+    {
+        output[count[(array[i] / exp) % 10] - 1] = array[i];
+        count[(array[i] / exp) % 10]--;
+    }
+
+    for (i = 0; i < array_size; i++)
+        array[i] = output[i];
+}
+
 void RadixSorter::sort(uint64_t *array, int array_size)
 {
-    // find MAX
-    //
-    std::vector<uint64_t> vec;
-    for (int i = 0; i < array_size; i++)
-    {
-        vec.push_back(array[i]);
-    }
-    std::sort(vec.begin(), vec.end());
-    for (int i = 0; i < array_size; i++)
-    {
-        array[i] = vec[i];
-    }
+    uint64_t m = getMax(array, array_size);
+    
+    for (uint64_t exp = 1; m / exp > 0; exp *= 10)
+        countSort(array, array_size, exp);
 }
 
 void ParallelRadixSorter::sort(uint64_t *array, int array_size)
 {
-    std::vector<uint64_t> vec;
-    for (int i = 0; i < array_size; i++)
-    {
-        vec.push_back(array[i]);
-    }
-    std::sort(vec.begin(), vec.end());
-    for (int i = 0; i < array_size; i++)
-    {
-        array[i] = vec[i];
-    }
+    uint64_t m = getMax(array, array_size);
+    
+    for (uint64_t exp = 1; m / exp > 0; exp *= 10)
+        countSort(array, array_size, exp);
 }
--- lab-1/ShellSorter.cc
+++ lab-1/ShellSorter.cc
@@ -24,9 +24,8 @@ void ShellSorter::sort(uint64_t *array, int array_size)
 
 void ParallelShellSorter::sort(uint64_t *array, int array_size)
 {
-
-    // Create thread handlers array
-    pthread_t *thread_handlers;
+    // Create thread array
+    pthread_t *threads;
 
     int gap = array_size / 2;
     for (int gap = array_size / 2; gap > 0; gap /= 2)
@@ -35,33 +34,37 @@ void ParallelShellSorter::sort(uint64_t *array, int array_size)
         for (int i = gap; i < array_size; i++)
         {
             // Clean up threads
-            if (thread_counter >= this->m_nthreads)
-            {
-                std::cout << "FREE" << std::endl;
-                for (int t_id = 0; t_id < this->m_nthreads; t_id++)
-                    pthread_join(thread_handlers[t_id], NULL);
-                thread_counter = 0;
-                free(thread_handlers);
-            }
+            for (int tid = 0; tid < this->m_nthreads; tid++)
+                pthread_join(threads[tid], NULL);
+            //free(threads);
 
-            thread_handlers = (pthread_t *)malloc(this->m_nthreads * sizeof(pthread_t));
-            for (int t_id = 0; t_id < this->m_nthreads; t_id++)
+            threads = (pthread_t *)malloc(this->m_nthreads * sizeof(pthread_t));
+            for (int tid = 0; tid < this->m_nthreads; tid++)
                 pthread_create(
-                    &thread_handlers[t_id],
+                    &threads[tid],
                     NULL,
                     &(this->thread_create_helper),
-                    new ParallelShellSorterArgs(this, t_id));
-            // uint64_t temp = array[i];
-            // int j;
-            // for (j = i; j >= gap && array[j - gap] > temp; j -= gap)
-            //     array[j] = array[j - gap];
-            // array[j] = temp;
+                    new ParallelShellSorterArgs(this, tid));
         }
+        // Clean up threads
+        for (int tid = 0; tid < this->m_nthreads; tid++)
+            pthread_join(threads[tid], NULL);
     }
+    // Clean up threads
+    for (int tid = 0; tid < this->m_nthreads; tid++)
+        pthread_join(threads[tid], NULL);
+    //free(threads);
 }
 
 void *ParallelShellSorter::thread_body(void *arg)
 {
-    std::cout << "Thread ID : " << ((ParallelShellSorterArgs *)arg)->tid << std::endl;
+    int tid = ((ParallelShellSorterArgs *)arg)->tid;
+    printf("%d", tid);
+
+    // uint64_t temp = array[i];
+    // int j;
+    // for (j = i; j >= gap && array[j - gap] > temp; j -= gap)
+    //     array[j] = array[j - gap];
+    // array[j] = temp;
     return NULL;
 }
