535d0678f6f6b9bed0414c6f62d6aec24b3a93d7
nekithockey75 <nikita01051997@gmail.com>
2020-04-06 15:59:30 +0500

    redundant mutex locks removed

--- lab-1/ShellSorter.cc
+++ lab-1/ShellSorter.cc
@@ -31,10 +31,8 @@ void ParallelShellSorter::sort(uint64_t *array, int array_size)
 
     for (int gap = array_size / 2; gap > 0; gap /= 2)
     {
-        pthread_mutex_lock(&gap_cycle_lock);
         this->gap = gap;
         gap_offset_i = 0;
-        pthread_mutex_unlock(&gap_cycle_lock);
 
         // Create threads
         for (int tid = 0; tid < m_nthreads; tid++)
@@ -55,9 +53,7 @@ void *ParallelShellSorter::thread_body(void *arg)
     int tid = ((ParallelShellSorterArgs *)arg)->tid;
     int gap, i, j;
 
-    pthread_mutex_lock(&gap_cycle_lock);
     gap = this->gap;
-    pthread_mutex_unlock(&gap_cycle_lock);
 
     while (true)
     {
