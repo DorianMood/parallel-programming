d418ff16585771be6a7205d5628fa4640d23d782
nekithockey75 <nikita01051997@gmail.com>
2020-04-26 16:06:15 +0500

    gitignore fixed

--- lab-2/tests.sh
+++ lab-2/tests.sh
@@ -9,19 +9,21 @@ else
     echo -e "\rBUILD ERROR :"
     echo $output
 fi
-'''
+
 # Run tests for K-Means
 for nobjects in 10000 100000 1000000; do
     for nclusters in 2 4 8 16; do
         # Sequential
+        echo "sequential";
         ./kmeans-serial -s $nobjects -k $nclusters
+        echo "parallel";
         # Parallels
         for nthreads in 1 2 4 8 16 32 64; do
             ./kmeans-parallel -s $nobjects -k $nclusters -n $nthreads
         done
     done
 done
-'''
+
 cd ..
 
 echo -ne "BUILDING OCEAN..."
