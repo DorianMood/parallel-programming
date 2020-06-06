49e2f062661dff2566a4980dbaec9268ca86cb76
dorianmood <dorianmood@163.com>
2020-06-06 17:10:19 +0800

    bfs serial

--- lab-4/bfs-template/bfsCPU.cpp
+++ lab-4/bfs-template/bfsCPU.cpp
@@ -1,6 +1,32 @@
+#include <queue>
+#include <iostream>
+
 #include "bfsCPU.h"
 
 void bfsCPU(int start, Graph &G, std::vector<int> &distance,
             std::vector<int> &parent, std::vector<bool> &visited) {
-    // PUT YOUR SERIAL CODE HERE
+
+    std::queue<int> q;
+
+    q.push(start);
+    visited[start] = true;
+    distance[start] = 0;
+    
+    while (!q.empty())
+    {
+        int v = q.front();
+        q.pop();
+        for (int i = 0; i < G.edgesSize[v]; i++)
+        {
+            int u = G.adjacencyList[G.edgesOffset[v] + i];
+            if (!visited[u])
+            {
+                parent[u] = v;
+                distance[u] = distance[v] + 1;
+
+                visited[u] = true;
+                q.push(u);
+            }
+        }
+    }
 }
